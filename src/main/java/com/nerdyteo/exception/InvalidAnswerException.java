bc0732e947e141ff693020ffb61c95b4f22b4b49
Arnold <arnoldteoguofong.gmail>
2023-04-29 14:59:52 +0800

    Fixed a bug in InvalidAnswerException where exception is thrown if a field of the answer object is not able to set to public

--- src/main/java/com/nerdyteo/exception/InvalidAnswerException.java
+++ src/main/java/com/nerdyteo/exception/InvalidAnswerException.java
@@ -4,15 +4,30 @@ import com.nerdyteo.Solution;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
 import java.util.function.Function;
 
 public class InvalidAnswerException extends RuntimeException {
 
-    private static final String MESSAGE_FORMAT = "\nInvalid Answer in [%s]. \n\t\tExpected Answer: %s, \n\t\tGiven Answer: %s";
-    private static final Function<Object, String> toString = object -> ToStringBuilder.reflectionToString(object, ToStringStyle.JSON_STYLE);
+    private static final String MESSAGE_FORMAT = "\nInvalid Answer in [%s]. \n\t\tExpected Answer: %s \n\t\tGiven Answer: %s";
+    private static Function<Object, String> toString = object -> {
+        if (object == null) {
+            return "null";
+        }
+
+        if (object.getClass().isArray()) {
+            return Arrays.toString((Object[]) object);
+        }
+        return object.toString();
+    };
 
     public InvalidAnswerException(Class<? extends Solution> solutionClass,
                                   Object expectedAnswer, Object givenAnswer) {
-        super(MESSAGE_FORMAT.formatted(solutionClass.getSimpleName(), toString.apply(expectedAnswer), toString.apply(givenAnswer)));
+        super(MESSAGE_FORMAT.formatted(solutionClass.getSimpleName(),
+                toString.apply(expectedAnswer),
+                toString.apply(givenAnswer)));
     }
 }
